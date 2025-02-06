91f6bee00feb0a4272a8e2a7fdc287a5d8c4c90c
muskanfatima5 <160497988+muskanfatima5@users.noreply.github.com>
2025-02-07 02:01:12 +0500

    Update page.tsx

--- app/components/Addtocart/page.tsx
+++ app/components/Addtocart/page.tsx
@@ -1,107 +1,15 @@
-// "use client";
-// import { useRouter } from "next/navigation";
-
-
-// const AddToCart: React.FC<any> = ({
-//   productId,
-//   productName,
-//   productPrice,
-//   productImage,
-// }) => {
-//   const router = useRouter();
-
-//   const handleAddToCart = () => {
-//     const cartItem = {
-//       productId,
-//       productName,
-//       productPrice,
-//       productImage,
-//     };
-
-//     const existingCart = JSON.parse(localStorage.getItem("cart") || "[]");
-//     existingCart.push(cartItem);
-
-//     localStorage.setItem("cart", JSON.stringify(existingCart));
-//     router.push("/cart");
-//   };
-
-//   return (
-//     <button
-//       onClick={handleAddToCart}
-//       className="bg-blue-600 text-white px-4 py-2 rounded-md mt-3 mb-[30px]"
-//     >
-//       Add to Cart
-//     </button>
-//   );
-// };
-
-// export default AddToCart;
-// "use client";
-// import { useRouter } from "next/navigation";
-
-// // Define a specific type for the props
-// interface addToCart {
-//   productId: string;
-//   productName: string;
-//   productPrice: number;
-//   productImage: string;
-// }
-
-// const AddToCart: React.FC<addToCart> = ({
-//   productId,
-//   productName,
-//   productPrice,
-//   productImage,
-// }) => {
-//   const router = useRouter();
-
-//   const handleAddToCart = () => {
-//     const cartItem = {
-//       productId,
-//       productName,
-//       productPrice,
-//       productImage,
-//     };
-
-//     const existingCart = JSON.parse(localStorage.getItem("cart") || "[]");
-//     existingCart.push(cartItem);
-
-//     localStorage.setItem("cart", JSON.stringify(existingCart));
-//     router.push("/cart");
-//   };
-
-//   return (
-//     <button
-//       onClick={handleAddToCart}
-//       className="bg-blue-600 text-white px-4 py-2 rounded-md mt-3 mb-[30px]"
-//     >
-//       Add to Cart
-//     </button>
-//   );
-// };
-
-// export default AddToCart;
-
-
-
-
-
-
-
-
-
 "use client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-type Cart = {
+type AddToCart = {
   productId: string;
   productName: string;
   productPrice: number;
   productImage: string;
 }
 
-const AddToCart: React.FC<Cart> = ({ productId, productName, productPrice, productImage }) => {
+const Cart: React.FC<AddToCart> = ({ productId, productName, productPrice, productImage }) => {
   const router = useRouter();
   const [quantity, setQuantity] = useState(1);
 
@@ -181,4 +89,4 @@ const AddToCart: React.FC<Cart> = ({ productId, productName, productPrice, produ
   );
 };
 
-export default AddToCart;
+export default Cart;
