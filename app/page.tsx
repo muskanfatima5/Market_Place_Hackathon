b31b27798fd65e11259af887a90bb605ed9bfa00
muskanfatima5 <160497988+muskanfatima5@users.noreply.github.com>
2025-02-07 01:35:15 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,6 @@
-// import { client } from "@/sanity/lib/client"
 import "./style/home.css"
 import Home from "./Home/page"
-// import product from "@/sanity/schemaTypes/product";
-// import { Product } from "./types/page";
 
-
-// async function Page(){
-//    const query = ` *[_type == "product"][3..10]
-//   {
-//      _id,
-//      name,
-//      price,
-//      description,
-//      category,
-//      discountPercent,
-//      "imageUrl":image.asset->url
-//  };`
-
-//   const products:Product[] = await client.fetch(query)
-//   console.log(products);
-
-//   return(
-//   <div className="main">
-//     {
-//     products.map((product:Product) => (
-//        <Home product={product}  key={product._id}/>
-//     ))}
-
-
-
-
-    
-    // <Home />
- 
-  // </div>)
 function Page(){
   return(
     <div className="main">
