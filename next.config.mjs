f255d5c6d2a3b46ab888760d003f2126507f96cc
muskanfatima5 <160497988+muskanfatima5@users.noreply.github.com>
2025-02-07 01:28:56 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,5 @@ export default {
   images: {
     domains: ['cdn.sanity.io'],
   },
-  eslint: {
-    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-  },
 }
 
